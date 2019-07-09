45bb0a3bd39564b04d9bae39f4333267880f341a
ChaoticStrings <44753506+ChaoticStrings@users.noreply.github.com>
2019-07-09 15:03:18 +0100

    Create raspieglobal.py

---
+++ raspieglobal.py
@@ -0,0 +1,28 @@
+state = "menu"
+
+while True:
+	global state
+	if state == "menu"
+		codigo do menu aqui
+		quando entra numa atividade
+		state = nome da atividade
+
+	if state == "bath"
+		codigo do banho
+		quando acaba o banho
+		state = "menu"
+	
+	if state == "eat"
+		codigo da comida
+		quando acaba a comida
+		state = "menu"
+
+	if state == "sleep"
+		codigo do sleep
+		quando acaba o dormir
+		state = "menu"
+
+	if state == "fun"
+		codigo do fun
+		quando acaba o fun
+		state == "menu"
