80c96d5a47e84905f79c14cce0089f29413e3e10
ChaoticStrings <44753506+ChaoticStrings@users.noreply.github.com>
2019-07-11 10:05:54 +0100

    Update funtime.py

--- funtime.py
+++ funtime.py
@@ -9,27 +9,24 @@ while True:
     o = sense.get_accelerometer_raw()['y']
     h = sense.get_accelerometer_raw()['z']
 
-y = 0
-x = 0
+    x = 0
+    y = 0
 
     d = round(x, 0)
     o = round(y, 0)
 
     if  o== -1:
-          y -= 1
+        y -= 1
     elif o == 1:
-         y += 1
+        y += 1
     elif d == 1:
         x +=  1
     elif d == -1
         x -= 1
 
-
-
-
-
 def draw_ball():
             sense.set_pixel(x,y,(183,125,63))
-            sense.set_pixel(6,6(249,247,77))
+            
+            
 
     if  == -1:
