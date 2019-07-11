2aeb7ae28354896c3d77267f80baefb85a6199e8
ChaoticStrings <44753506+ChaoticStrings@users.noreply.github.com>
2019-07-11 10:09:04 +0100

    Update funtime.py

--- funtime.py
+++ funtime.py
@@ -15,18 +15,35 @@ while True:
     d = round(x, 0)
     o = round(y, 0)
 
-    if  o== -1:
-        y -= 1
+    if  o == -1:
+        def move_up(event):
+            global y
+            sense.set_pixel(x,y, (0,0,0))
+            if event.action == 'pressed' and y > 0:
+                y -= 1
+        sense.stick.direction_up = move_up
     elif o == 1:
-        y += 1
+        def move_down(event):
+            global y
+            sense.set_pixel(x,y, (0,0,0))
+            if event.action == 'pressed' and y < 7:
+                y += 1
+        sense.stick.direction_down = move_down
     elif d == 1:
-        x +=  1
-    elif d == -1
-        x -= 1
+        def move_left(event):
+            global x
+            sense.set_pixel(x,y, (0,0,0))
+            if event.action == 'pressed' and x > 0:
+                x -= 1
+        sense.stick.direction_left = move_left
+    elif d == -1:
+       def move_right(event):
+            global x
+            sense.set_pixel(x,y, (0,0,0))
+            if event.action == 'pressed' and x < 7:
+                x += 1
+        sense.stick.direction_right = move_right        
+    draw_ball()
 
 def draw_ball():
             sense.set_pixel(x,y,(183,125,63))
-            
-            
-
-    if  == -1:
